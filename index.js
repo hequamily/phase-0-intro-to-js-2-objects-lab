3b42d0652870a1b533fa0521f41846355d7da442
Harrison Quamily <quamilyh@gmail.com>
2023-03-13 20:32:27 -0400

    objects

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+const employee = {
+    id: 1234,
+    name: 'John Doe',
+    department: 'Sales',
+    salary: 90000
+  };
+  
+
+    const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'name', 'Jane Doe');
+    
+  
+    expect(updatedEmployee).toEqual({
+      id: 1234,
+      name: 'Jane Doe',
+      department: 'Sales',
+      salary: 90000
+    });
+
+
+    function updateEmployeeWithKeyAndValue(employee, key, value) {
+        const updatedEmployee = { ...employee };
+        updatedEmployee[key] = value;
+        return updatedEmployee;
+      }
+      function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+      }
+      function deleteFromEmployeeByKey(employee, key) {
+        const updatedEmployee = { ...employee };
+        delete updatedEmployee[key];
+        return updatedEmployee;
+      }
+      
+      function destructivelyDeleteFromEmployeeByKey(employee, key) {
+        delete employee[key];
+        return employee;
+      }
+      
